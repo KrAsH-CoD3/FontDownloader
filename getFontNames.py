c5dda25ef36dc4332073c1bfa9667748cab02c37
Samm <44311301+KrAsH-CoD3@users.noreply.github.com>
2025-08-02 06:33:34 +0100

    Updated output path

--- getFontNames.py
+++ getFontNames.py
@@ -1,11 +1,11 @@
 import os
 
 #  Get font names from folder and remove extension
-directory_path = r"Path/to/fonts"
+directory_path = r"C:\Users\LMAO\AppData\Local\camoufox\camoufox\Cache\fonts"
 
 file_names = [f for f in os.listdir(directory_path) if os.path.isfile(os.path.join(directory_path, f))]
 
-with open('Output/path/to/fonts_names.txt', 'w', encoding='utf-8') as f:
+with open(r'Fonts\Camoufox\Fonts_names.txt', 'w', encoding='utf-8') as f:
     for name in file_names:
         new_name = name.split('.')[0]
         f.write(new_name + '\n')
